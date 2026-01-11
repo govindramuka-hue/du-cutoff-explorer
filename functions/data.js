500252cabf610de69907a58a10efdcaa335fbff4
govindramuka-hue <govindramuka@gmail.com>
2026-01-11 21:08:43 +0530

    Update data.js

--- functions/data.js
+++ functions/data.js
@@ -1,7 +1,23 @@
 import data from "./data.json";
 
-export async function onRequestGet() {
-  return new Response(JSON.stringify(data), {
+export async function onRequestGet({ request }) {
+  const url = new URL(request.url);
+
+  const program = url.searchParams.get("program");
+  const category = url.searchParams.get("category");
+
+  if (!program || !category) {
+    return new Response("Forbidden", { status: 403 });
+  }
+
+  const result = data
+    .filter(d => d["PROGRAM NAME"] === program)
+    .map(d => ({
+      college: d["COLLEGE NAME"],
+      cutoff: d[category]
+    }));
+
+  return new Response(JSON.stringify(result), {
     headers: { "Content-Type": "application/json" }
   });
 }
