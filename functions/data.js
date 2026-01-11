4577b65b43de2e8e5c7280d09afec809e605f5df
govindramuka-hue <govindramuka@gmail.com>
2026-01-11 21:11:34 +0530

    Update data.js

--- functions/data.js
+++ functions/data.js
@@ -1,23 +1,7 @@
 import data from "./data.json";
 
-export async function onRequestGet({ request }) {
-  const url = new URL(request.url);
-
-  const program = url.searchParams.get("program");
-  const category = url.searchParams.get("category");
-
-  if (!program || !category) {
-    return new Response("Forbidden", { status: 403 });
-  }
-
-  const result = data
-    .filter(d => d["PROGRAM NAME"] === program)
-    .map(d => ({
-      college: d["COLLEGE NAME"],
-      cutoff: d[category]
-    }));
-
-  return new Response(JSON.stringify(result), {
+export async function onRequestGet() {
+  return new Response(JSON.stringify(data), {
     headers: { "Content-Type": "application/json" }
   });
 }
